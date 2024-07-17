fcc420b0ec672954ebbcde78aa7a022cf61cd493
Shayne999 <shaynejuniorn@gmail.com>
2024-07-17 12:47:12 +0200

    1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Creates a Cache class."""
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -18,6 +18,23 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, float, None]:
+            """Retrieve data from Redis and optionally apply a conversion function"""
+            data = self._redis.get(key)
+            if data is not None and fn is not None:
+                return fn(data)
+            return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """Retrieve data from Redis and convert it to a string"""
+        data = self.get(key, fn=lambda d: d.decode("utf-8"))
+        return data
+
+    def get_int(self, key: str) -> Optional[int]:
+        """Retrieve data from Redis and convert it to an integer"""
+        data = self.get(key, fn=int)
+        return data
 
 
 # if __name__ == "__main__":
