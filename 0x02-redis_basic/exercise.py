ba3c908bce18ee74652ca42b7b60cf0bdadf6951
Shayne999 <shaynejuniorn@gmail.com>
2024-07-17 12:39:44 +0200

    0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -20,12 +20,12 @@ class Cache:
         return key
 
 
-if __name__ == "__main__":
-    cache = Cache()
+# if __name__ == "__main__":
+#     cache = Cache()
 
-    data = b"hello"
-    key = cache.store(data)
-    print(key)
+#     data = b"hello"
+#     key = cache.store(data)
+#     print(key)
 
-    local_redis = redis.Redis()
-    print(local_redis.get(key))
+#     local_redis = redis.Redis()
+#     print(local_redis.get(key))
